7e4e2c836507659f56ea1a60e3b181f4977bc5ac
jrburke <jrburke@gmail.com>
2012-10-17 16:43:02 -0700

    export found macros in a very hacky way

--- src/expander.js
+++ src/expander.js
@@ -61,14 +61,14 @@
 
     // var CContext = or(Null, object({
     //     mark: opt(Num),
-    //     // id: opt(CSyntax), 
+    //     // id: opt(CSyntax),
     //     name: opt(Str),
     //     context: or(Null, Self)
     // }));
 
     // var CSyntax = object({
     //     token: CToken,
-    //     context: CContext 
+    //     context: CContext
     // });
 
     // var CVar = object({
@@ -100,7 +100,7 @@
 
     // probably a more javascripty way than faking constructors but screw it
     // (Num) -> CContext
-    function Mark(mark, ctx) { 
+    function Mark(mark, ctx) {
         return {
             mark: mark,
             context: ctx
@@ -127,14 +127,14 @@
         };
     }
 
-    var isRename = function(r) { 
+    var isRename = function(r) {
         return r && (typeof r.id !== 'undefined') && (typeof r.name !== 'undefined');
     }
 
     function DummyRename(name, ctx) {
         return {
             dummy_name: name,
-            context: ctx      
+            context: ctx
         };
     }
 
@@ -200,7 +200,7 @@
                 swappedToken.inner = swappedInner;
             }
 
-            return syntaxFromToken(swappedToken, 
+            return syntaxFromToken(swappedToken,
                                     renameDummyCtx(this.context, ident, name, dummyName));
         }
     };
@@ -208,19 +208,19 @@
     function renameDummyCtx(ctx, ident, name, dummyName) {
         if(ctx === null) {
             return null;
-        } 
+        }
         if(isDummyRename(ctx) && ctx.dummy_name === dummyName) {
             return Rename(ident, name, DummyRename(ctx.dummy_name, ctx.context));
-        } 
+        }
         if(isDummyRename(ctx) && ctx.dummy_name !== dummyName) {
             return DummyRename(ctx.dummy_name, renameDummyCtx(ctx.context, ident, name, dummyName));
-        } 
+        }
         if(isMark(ctx)) {
             return Mark(ctx.mark, renameDummyCtx(ctx.context, ident, name, dummyName));
-        } 
+        }
         if(isRename(ctx)) {
             return Rename(ctx.id, ctx.name, renameDummyCtx(ctx.context, ident, name, dummyName));
-        } 
+        }
         parser.assert(false, "expecting a fixed set of context types");
     }
 
@@ -261,10 +261,10 @@
             mark = stx.context.mark;
             submarks = marksof(syntaxFromToken(stx.token, stx.context.context));
             return remdup(mark, submarks);
-        } 
+        }
         if(isRename(stx.context) || isDummyRename(stx.context)) {
             return marksof(syntaxFromToken(stx.token, stx.context.context));
-        } 
+        }
         return [];
     }
 
@@ -272,7 +272,7 @@
     function resolve(stx) {
         if(isMark(stx.context) || isDummyRename(stx.context)) {
             return resolve(syntaxFromToken(stx.token, stx.context.context));
-        } 
+        }
         if (isRename(stx.context)) {
             var idName = resolve(stx.context.id);
             var subName = resolve(syntaxFromToken(stx.token, stx.context.context));
@@ -282,9 +282,9 @@
 
             if((idName === subName) && (_.difference(idMarks, subMarks).length === 0)) {
                 return stx.token.value + stx.context.name;
-            } 
+            }
             return resolve(syntaxFromToken(stx.token, stx.context.context));
-        } 
+        }
         return stx.token.value;
     }
 
@@ -306,7 +306,7 @@
                 token.inner = tokensToSyntax(token.inner);
             }
             return syntaxFromToken(token);
-        }); 
+        });
     }
 
     // ([...CSyntax]) -> [...CToken]
@@ -322,7 +322,7 @@
 
     // CToken -> Bool
     function isPatternVar(token) {
-        return token.type === parser.Token.Identifier 
+        return token.type === parser.Token.Identifier
             && token.value[0] === "$"   // starts with $
             && token.value !== "$";     // but isn't $
     }
@@ -332,7 +332,7 @@
         return _.any(patterns, function(pat) {
             if(pat.token.type === parser.Token.Delimiter) {
                 return containsPatternVar(pat.token.inner);
-            } 
+            }
             return isPatternVar(pat);
         });
     }
@@ -353,7 +353,7 @@
                     if(last && isPatternVar(last.token)) {
                         return acc;
                     }
-                } 
+                }
                 if(last && last.token.value === ":") {
                     if(lastLast && isPatternVar(lastLast.token)) {
                         return acc;
@@ -396,7 +396,7 @@
                 }
 
                 // skip over ... and (,)
-                if(patStx.token.value === "..." 
+                if(patStx.token.value === "..."
                         || (delimIsSeparator(patStx) && next && next.token.value === "...")) {
                     return acc;
                 }
@@ -435,8 +435,8 @@
             }
         } else {
             try {
-                var parseResult = parser.parse(callStx, 
-                                                pattern.class, 
+                var parseResult = parser.parse(callStx,
+                                                pattern.class,
                                                 {tokens:true});
 
                 consumed = parseResult.tokens.length;
@@ -514,12 +514,12 @@
                         // parser.assert(false, "sub delimiters in a macro pattern not supported right now, to be fixed with: https://github.com/mozilla/sweet.js/issues/28");
                         var subEnv = buildPatternEnv(callStx[callIdx].token.inner, pattern.token.inner);
                         consumed = subEnv.consumed;
-                        if(consumed !== callStx[callIdx].token.inner.length && repeat === false) { 
+                        if(consumed !== callStx[callIdx].token.inner.length && repeat === false) {
                             matchFailed = true;
                             callIdx = 0;
                             break;
                         }
-                        // consumed is the number of tokens inside the delimiter but we just need to 
+                        // consumed is the number of tokens inside the delimiter but we just need to
                         // move forward one token on this level
                         callIdx += (consumed > 0) ? 1 : 0;
                     }
@@ -580,7 +580,7 @@
                     }
                 }
 
-                // if we matched no tokens note that the match failed 
+                // if we matched no tokens note that the match failed
                 // so we don't check the remaining patterns and break out
                 // of the repeat loop
                 if(consumed === 0) {
@@ -666,7 +666,7 @@
 
     // ([...[...CSyntax]], Str) -> [...CSyntax]
     function joinSyntaxArr(tojoin, punc) {
-        if(tojoin.length === 0) { return []; } 
+        if(tojoin.length === 0) { return []; }
         if(punc === " ") {
             return _.flatten(tojoin, true);
         }
@@ -678,7 +678,7 @@
 
     // (CSyntax) -> Bool
     function delimIsSeparator(delim) {
-        return (delim && delim.token.type === parser.Token.Delimiter 
+        return (delim && delim.token.type === parser.Token.Delimiter
                 && delim.token.value === "()"
                 && delim.token.inner.length === 1
                 && delim.token.inner[0].token.type !== parser.Token.Delimiter
@@ -718,11 +718,11 @@
 
         var matches = _.chain(_.zip(callSyntax, patterns))
                         .map(function(ziped) {
-                            var call = ziped[0], 
+                            var call = ziped[0],
                                 pat = ziped[1];
 
                             if (pat.token.type === parser.Token.Delimiter) {
-                                if(!(call.token.type === parser.Token.Delimiter 
+                                if(!(call.token.type === parser.Token.Delimiter
                                     && call.token.value === pat.token.value)) {
                                     numberMatched = 0;
                                     return {};
@@ -766,7 +766,7 @@
     function mkMacroTransformer(macroCases) {
         // grab the patterns from each case and sort them by longest number of patterns
         var sortedCases = _.sortBy(macroCases, function(mcase) {
-                                    return patternLength(mcase.pattern); 
+                                    return patternLength(mcase.pattern);
                                 }).reverse();
 
         return function(callSyntax, macroNameStx) {
@@ -845,7 +845,7 @@
 
                                 var repeatLength = env[nonScalar].match.length;
                                 var sameLength = _.all(fv, function(pat) {
-                                    return (env[pat].level === 0) 
+                                    return (env[pat].level === 0)
                                             || (env[pat].match.length === repeatLength);
                                 });
                                 parser.assert(sameLength, "all non-scalars must have the same length");
@@ -890,12 +890,12 @@
                                 var newBody = syntaxFromToken(_.clone(bodyStx.token), bodySyntax.context);
                                 newBody.token.inner = transcribe(bodyStx.token.inner, env);
                                 return acc.concat(newBody);
-                            } 
+                            }
                             if(env[bodyStx.token.value]) {
                                 parser.assert(env[bodyStx.token.value].level === 0, "match ellipses level does not match");
-                                return acc.concat(takeLineContext(macroNameStx, 
+                                return acc.concat(takeLineContext(macroNameStx,
                                                                   env[bodyStx.token.value].match));
-                            } 
+                            }
                             return acc.concat(takeLineContext(macroNameStx, [bodyStx]));
                         }
                     }, []).value();
@@ -965,7 +965,7 @@
                     lineNumber: stx.token.endLineNumber,
                     lineStart: stx.token.endLineStart
                 }, stx.context));
-            } 
+            }
             return acc.concat(stx);
         }, []);
     }
@@ -987,7 +987,7 @@
 
     // (CSyntax) -> [...CSyntax]
     function getArgList(argSyntax) {
-        parser.assert(argSyntax.token.type === parser.Token.Delimiter, 
+        parser.assert(argSyntax.token.type === parser.Token.Delimiter,
             "expecting delimiter for function params");
         return _.filter(argSyntax.token.inner, function(stx) {
             return stx.token.value !== ",";
@@ -1017,19 +1017,19 @@
             var atFunctionDelimiter;
 
             if (curr.token.type === parser.Token.Delimiter) {
-                atFunctionDelimiter = (curr.token.value === "()" && (isFunctionStx(body[idx-1]) 
+                atFunctionDelimiter = (curr.token.value === "()" && (isFunctionStx(body[idx-1])
                                                                   || isFunctionStx(body[idx-2]))) ||
                                       (curr.token.value === "{}" && (isFunctionStx(body[idx-2])
                                                                   || isFunctionStx(body[idx-3])))
                 // don't look for var idents inside nested functions
                 if(!atFunctionDelimiter) {
                     return acc.concat(getVarIdentifiers(curr.token.inner));
-                } 
+                }
                 return acc;
-            } 
+            }
             if (isVarStx(body[idx-1])) {
-                var parseResult = parser.parse(flatten(body.slice(idx)), 
-                                            "VariableDeclarationList", 
+                var parseResult = parser.parse(flatten(body.slice(idx)),
+                                            "VariableDeclarationList",
                                             {noresolve: true});
                 return acc.concat(varNamesInAST(parseResult));
             }
@@ -1082,7 +1082,8 @@
                 var macroBody = stx[index++].token;
 
                 if(doneLoadingMacroDefs) {
-                    parser.assert(false, "all macros must be defined at the top of the scope");
+                    //JRB CHEATING to allow `export macro`
+                    //parser.assert(false, "all macros must be defined at the top of the scope");
                 }
 
                 parser.assert(macroBody.value === "{}", "expecting a macro body");
@@ -1100,7 +1101,7 @@
                     consumeRange = _.range(macroDef.toConsume);
                 }
                 var callArgs = _.map(consumeRange, function() {
-                    parser.assert(!(stx[index].token.type === parser.Token.Punctuator && stx[index].token.value === ","), 
+                    parser.assert(!(stx[index].token.type === parser.Token.Punctuator && stx[index].token.value === ","),
                         "commas are not allowed in macro call");
                     return stx[consumeIdx++];
                 });
@@ -1125,7 +1126,7 @@
                 if(argsDelim.token.type === parser.Token.Identifier) {
                     functionName = argsDelim;
                     argsDelim = stx[index++];
-                } 
+                }
                 var bodyDelim = stx[index++];
 
                 parser.assert(argsDelim.token.type === parser.Token.Delimiter, "expecting delimiter for function params");
@@ -1143,7 +1144,7 @@
                 var renamedArgs = _.map(freshnameArgPairs, function(argPair) {
                     var freshName = argPair[0];
                     var arg = argPair[1];
-                    return arg.rename(arg, freshName); 
+                    return arg.rename(arg, freshName);
                 });
 
                 var newEnv = _.reduce(_.zip(freshNames, renamedArgs), function (accEnv, argPair) {
@@ -1172,7 +1173,7 @@
 
                 var varIdents = getVarIdentifiers(flatBody);
                 varIdents = _.filter(varIdents, function(varId) {
-                    // only pick the var identifiers that are not 
+                    // only pick the var identifiers that are not
                     // resolve equal to a parameter of this function
                     return !(_.any(renamedArgs, function(param) {
                         return resolve(varId) === resolve(param);
@@ -1210,7 +1211,7 @@
                     ident = env[resolvedIdent].id;
                 } else {
                     // todo is this right?
-                    // ident = mkSyntax(resolvedIdent, Token.Identifier, currStx); 
+                    // ident = mkSyntax(resolvedIdent, Token.Identifier, currStx);
                     ident = currStx
                 }
                 expanded = expanded.concat(ident);
@@ -1224,10 +1225,15 @@
             }
         }
 
+        exports.foundMacros = macros;
+
         return expanded;
     }
 
-    exports.expand = expand;
+    exports.expand = function () {
+        exports.foundMacros = {};
+        return expand.apply(null, arguments);
+    };
     exports.resolve = resolve;
     exports.flatten = flatten;
     exports.tokensToSyntax = tokensToSyntax;
